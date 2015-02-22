a7e381ac19d43ad3427b79b2f7725daa755ba1ac
hinatami <suzuming+git@gmail.com>
2015-02-22 17:51:52 +0900

    コードレビューを参考に、一部修正しました。

--- promote-request-kindle/promote-request-kindle.user.js
+++ promote-request-kindle/promote-request-kindle.user.js
@@ -6,19 +6,15 @@
 // @grant       none
 // ==/UserScript==
 
-window.onload = function() {
+document.addEventListener('DOMContentLoaded', function(){
 	var kindleBlock = document.getElementById('fiona-publisher-signup-link');
-	var kindleChildren = kindleBlock.children;
-	for (var i = 0; i < kindleChildren.length; i++) {
-	  if (kindleChildren[i].innerHTML.match(/Kindle化リクエスト/)) {
-	    migrateKindle();
-	    break;
-	  }
+	if (kindleBlock && kindleBlock.innerHTML.match(/Kindle化リクエスト/)) {
+		migrateKindle();
 	}
 
 	function migrateKindle(){
-	  var insertBox = document.getElementById('rightRail');
-	  var rightCol = document.getElementById('rightCol');
-	  rightCol.insertBefore(insertBox, rightCol.firstChild);  
+		var insertBox = document.getElementById('rightRail');
+		var rightCol = document.getElementById('rightCol');
+		rightCol.insertBefore(insertBox, rightCol.firstChild);  
 	}
-}
+});

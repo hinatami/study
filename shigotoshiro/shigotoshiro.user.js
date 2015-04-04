bf0aecc260cba60175dbb48bc599d59b1f86a54b
hinatami <suzuming@gmail.com>
2015-04-04 16:14:48 +0900

    fixed #3
    時刻判定部分の書き方を修正

--- shigotoshiro/shigotoshiro.user.js
+++ shigotoshiro/shigotoshiro.user.js
@@ -22,19 +22,26 @@ $(function () {
   */
 
 	var today = new Date();
+
 	// 今日の曜日を取得します
 	var day = today.getDay();
-	// 今の時間を分換算で取得します
-	var time = today.getHours()*60 + today.getMinutes();
-
 	// 曜日が「0:日曜日」か「6:土曜日」だったら何もしない
 	if (~[0, 6].indexOf(day)) {
 		return;
 	}
-	// 時間が「9時〜18時」であることを判別して
-	if (9*60 <= time && time <= 18*60) {
-		// body要素の最後に「見れなくする」ための要素を置きます
-		var $htmlBody = $('body');
-		$htmlBody.append('<div style="position:fixed;top:0;left:0;width:100%;height:100%;background-color:rgba(0,0,0,.85);color:#fff;font-size:3rem;text-align:center;z-index:500;"><p style="margin-top:5em;">仕事しよ？ (´・ω・`)</p></div>');
+
+	// 今の時間を取得します
+	var time = today.getHours();
+	// 時間が「9時台〜18時台」以外は除外
+	if (time < 9 || time > 18) {
+		return;
 	}
+	// 時間が「18時01分以降」なら除外（本当は18時ちょうどでも見れていいらしいけど、せっかくなので入れたままにしておきます）
+	if (time === 18 && today.getMinutes() > 0) {
+		return;
+	}
+
+	// body要素の最後に「見れなくする」ための要素を置きます
+	var $htmlBody = $('body');
+	$htmlBody.append('<div style="position:fixed;top:0;left:0;width:100%;height:100%;background-color:rgba(0,0,0,.85);color:#fff;font-size:3rem;text-align:center;z-index:500;"><p style="margin-top:5em;">仕事しよ？ (´・ω・`)</p></div>');
 });
